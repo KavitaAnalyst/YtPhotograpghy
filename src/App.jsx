e3287bdaca577556abb92cda226833960ee302d7
KavitaAnalyst <kaviitatripathi@gmail.com>
2024-12-19 18:00:31 +0530

    changes made to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import About from "./pages/About";
 import Gallery from "./pages/Gallery";
+// import Blogs from "./pages/Blogs";
+// import Animals from "./pages/Animals";
+// import Birds from "./pages/Animals";
+// import Nature from "./pages/Nature";
 
 function App() {
   return (
@@ -12,6 +16,11 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery />} />
+        {/* <Route path="/blogs" element={<Blogs />} /> */}
+        {/* <Route path="/animals" element={<Animals />} />
+        <Route path="/birds" element={<Birds />} />
+        <Route path="/nature" element={<Nature />} /> */}
+
       </Routes>
     </Router>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,33 @@
 // src/components/Navbar.js
 // import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/LOGO.png";
 import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">Nature Photography</div>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/gallery">Gallery</Link></li>
-      </ul>
+      <div className="logo">
+        <Link to="/"><img src = {logo} alt="Logo" style={{ height: '100px', marginRight: '10px', marginTop:'10px'}} /></Link>
+      </div>
+      <div>
+      <ul className="nav-links" style={{ listStyle: 'none', padding: '0', margin: '0', textAlign: 'center' }}>
+      {/* <li><Link to="/">Home</Link></li> */}
+      <li><Link to="/about">About</Link></li>
+      <li className="dropdown">
+        <Link to="/gallery">Portfolio</Link>
+        <ul className="dropdown-menu" style={{ listStyle: 'none', padding: '0', margin: '0', display: 'none', position: 'absolute', backgroundColor: '#fff', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
+          <li><Link to="/animals" style={{ display: 'block', padding: '10px', textDecoration: 'none' }}>Animals</Link></li>
+          <li><Link to="/birds" style={{ display: 'block', padding: '10px', textDecoration: 'none' }}>Birds</Link></li>
+          <li><Link to="/nature" style={{ display: 'block', padding: '10px', textDecoration: 'none' }}>Nature</Link></li>
+        </ul>
+      </li> 
+      <li><Link to="/blogs">Blogs</Link></li>
+    </ul>
+      </div>
+      
     </nav>
   );
 }
-
 export default Navbar;
+
